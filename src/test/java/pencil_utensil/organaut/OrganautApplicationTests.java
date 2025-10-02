9f2fc8bb34f9bf73cd52ccd149ee9e782590611e
alldaygooning@gmail.com <alldaygooning@gmail.com>
2025-10-02 17:19:35 +0300

    Initial commit

---
+++ src/test/java/pencil_utensil/organaut/OrganautApplicationTests.java
@@ -0,0 +1,13 @@
+package pencil_utensil.organaut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganautApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
