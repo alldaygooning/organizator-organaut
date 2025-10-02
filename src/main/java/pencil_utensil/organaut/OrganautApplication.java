fe800e0a261847a264158ee593472c92cf134e1b
alldaygooning <nikich1402@mail.ru>
2025-10-02 17:19:35 +0300

    Initial commit

---
+++ src/main/java/pencil_utensil/organaut/OrganautApplication.java
@@ -0,0 +1,13 @@
+package pencil_utensil.organaut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganautApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganautApplication.class, args);
+	}
+
+}
